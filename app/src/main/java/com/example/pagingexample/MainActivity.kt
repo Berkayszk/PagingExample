0091e0bad7cfe849c0e506b68b55e195690764ed
Berkay Sazak <berkaysazak313@gmail.com>
2023-02-28 15:46:00 +0300

    Complete!!

--- app/src/main/java/com/example/pagingexample/MainActivity.kt
+++ app/src/main/java/com/example/pagingexample/MainActivity.kt
@@ -2,12 +2,47 @@ package com.example.pagingexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.pagingexample.adapter.RickMortyAdapter
+import com.example.pagingexample.databinding.ActivityMainBinding
+import com.example.pagingexample.viewmodel.RickMortyViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var mAdapter: RickMortyAdapter
+    private val viewModel: RickMortyViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpRv()
+        loadingData()
+    }
+
+    private fun loadingData(){
+        lifecycleScope.launch {
+            viewModel.listData.collect{pagingData ->
+                mAdapter.submitData(pagingData)
+            }
+        }
+    }
+    private fun setUpRv(){
+        mAdapter = RickMortyAdapter()
+        binding.recyclerView.apply {
+
+            layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+            adapter = mAdapter
+
+            setHasFixedSize(true)
+
+        }
+
     }
 }
